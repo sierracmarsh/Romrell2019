69940ff5a8ba224652668653175608b2123ae1ad
Sierra Marsh <ierr.mar@gmail.com>
2019-01-30 10:43:44 -0700

    First game sprites and physics

---
+++ FirstGame/Assets/deathwheelevent.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+public class deathwheelevent : MonoBehaviour
+{
+	public UnityEvent Event;
+
+
+	private void OnCollisionEnter2D()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	() {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
