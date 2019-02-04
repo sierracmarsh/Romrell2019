4d2ed8fd2e15dbd3dbbcda685927711c0b4ef09a
Sierra Marsh <ierr.mar@gmail.com>
2019-02-04 11:07:11 -0700

    More 2/4/2019

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Animal : MonoBehaviour {
-	
-	public string Name:
-	
-	public Color SkinColor:
+public class Animal : MonoBehaviour
+{
+
+	public string Name;
+
+	public Color SkinColor;
 
 	// Use this for initialization
 	void Start () {
