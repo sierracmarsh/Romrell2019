9844afb93177eca1a6b355be2bd7e18259e462ba
Sierra Marsh <ierr.mar@gmail.com>
2019-02-04 10:59:55 -0700

    Class 2/4/2019

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+	
+	public string Name:
+	
+	public Color SkinColor:
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
