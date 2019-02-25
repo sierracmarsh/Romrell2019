1ba48572d7e3ea31af39e901ee265b717068790d
Sierra Marsh <ierr.mar@gmail.com>
2019-02-25 11:33:47 -0700

    Class 2/25
    If else, make switch

---
+++ FirstGame/Assets/Scripts/Condition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Condition : MonoBehaviour
+{
+	public bool CanWalk;
+	public int Number;
+	public string Password;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CanWalk)
+		{
+			print("True");
+		}
+
+		if (Number >= 10)
+		{
+			print("Math Sucks Dick");
+		}
+	}
+}
