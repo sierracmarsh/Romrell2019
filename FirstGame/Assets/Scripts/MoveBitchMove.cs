57f2363a52ac3bcdcd297ef806d979dbdb5fc5a8
Sierra Marsh <ierr.mar@gmail.com>
2019-02-27 11:13:40 -0700

    Boop
    Character Controller, I guess Inverted cross is my main. Except he keeps flying away. Sigh

---
+++ FirstGame/Assets/Scripts/MoveBitchMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class MoveBitchMove : MonoBehaviour
+{
+    public UnityEvent OnGrounded, OffGrounded;
+	public int Speed = 3;
+	
+	public CharacterController controller;
+	private Vector3 position;
+
+	
+	void Start ()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+	
+	
+	void Update ()
+	{
+        if(controller.isGrounded)
+        {
+            OnGrounded.Invoke();
+        }
+        else
+        {
+            OffGrounded.Invoke();
+        }
+        position.y = Input.GetAxis("Vertical") + Speed * Time.deltaTime;
+
+        position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+		
+		
+		controller.Move(position);
+	}
+}
