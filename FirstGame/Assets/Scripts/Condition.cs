49aa6df344ea2b39c2b325807e28d7cd47e7a6f0
Sierra Marsh <ierr.mar@gmail.com>
2019-02-27 10:18:06 -0700

    I MADE A FUCKING LIGHT
    and I did it the hard way
    lol

--- FirstGame/Assets/Scripts/Condition.cs
+++ FirstGame/Assets/Scripts/Condition.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Condition : MonoBehaviour
 {
-	public bool CanWalk;
-	public int Number;
+	public UnityEvent OnEvent, OffEvent;
+	public bool OnBool;
 	public string Password;
 	
 
@@ -16,14 +17,14 @@ public class Condition : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-		if (CanWalk)
+		if (OnBool)
 		{
-			print("True");
+			OnEvent.Invoke();
+		}
+		else
+		{
+			OffEvent.Invoke();
 		}
 
-		if (Number >= 10)
-		{
-			print("Math Sucks Dick");
-		}
 	}
 }
---
+++ FirstGame/Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//I tookt hsi code off of UnityAnswers because I'm stupid
+public class LightSwitch : MonoBehaviour {
+
+
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+			this.GetComponent<Light>().enabled = true;
+		if (Input.GetKey(KeyCode.DownArrow))
+			this.GetComponent<Light>().enabled = false;
+
+	}
+}
