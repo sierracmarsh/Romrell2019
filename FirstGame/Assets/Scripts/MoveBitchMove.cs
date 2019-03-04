a7ab2ffa758a496ecbbdc4e63dbbffbe92993b26
Sierra Marsh <ierr.mar@gmail.com>
2019-03-04 11:15:41 -0700

    Need to finish Class

--- FirstGame/Assets/Scripts/MoveBitchMove.cs
+++ FirstGame/Assets/Scripts/MoveBitchMove.cs
@@ -6,34 +6,53 @@ using UnityEngine.Events;
 
 public class MoveBitchMove : MonoBehaviour
 {
-    public UnityEvent OnGrounded, OffGrounded;
-	public int Speed = 3;
-	
+	public UnityEvent OnGrounded, OffGrounded;
+	public int Speed = 5;
+	public float Gravity = 10f;
+	public float JumpForce = 5;
+
 	public CharacterController controller;
 	private Vector3 position;
 
-	
-	void Start ()
+
+	void Start()
 	{
 		controller = GetComponent<CharacterController>();
 	}
-	
-	
-	void Update ()
-	{
-        if(controller.isGrounded)
-        {
-            OnGrounded.Invoke();
-        }
-        else
-        {
-            OffGrounded.Invoke();
-        }
-        position.y = Input.GetAxis("Vertical") + Speed * Time.deltaTime;
 
-        position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+
+	void Update()
+	{
+		if (controller.isGrounded)
+		{
+			OnGrounded.Invoke();
+		}
+		else
+		{
+			OffGrounded.Invoke();
+		}
+
+		if (controller.isGrounded && Input.GetButton("Jump"))
+		{
+			position.y = JumpForce*Time.deltaTime;
+		}
 		
-		
+
+//		if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+//
+//		{
+//			position.y = Input.GetAxis("Vertical") + Speed * Time.deltaTime;
+//		}
+//		else
+//		{
+//			position.y = Gravity * Time.deltaTime;
+//		}
+
+		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+
+
+
 		controller.Move(position);
 	}
 }
+
---
+++ FirstGame/Assets/Scripts/MoveSingleJump.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSingleJump : MoveBase {
+
+	public AnimatorOverrideController void Move CharacterController controller
