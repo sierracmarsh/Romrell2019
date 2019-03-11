b29bdbbc2ae8f495a0def6a7dce61c11d590ff35
Sierra Marsh <ierr.mar@gmail.com>
2019-03-11 11:21:57 -0600

    Health Bar, unfinished

---
+++ FirstGame/Assets/Scripts/UpdateBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateBar : MonoBehaviour
+{
+	private Image BarImage;
+	public FloatData FillNumber;
+
+	// Use this for initialization
+	void Start ()
+	{
+		BarImage = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		BarImage.fillAmount = FillNumber.Value;
+	}
+}
