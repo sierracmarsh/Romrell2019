8563871d7f535f69d3027bf26f5d0e7c113b1647
Sierra Marsh <ierr.mar@gmail.com>
2019-04-01 11:21:59 -0600

    Switch Events
    Super Useful, review later

---
+++ FirstGame/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class StateMachine : ScriptableObject
+{
+	public UnityEvent StartingEvent, PlayingEvent, EndingEvent;
+	public enum States
+	{
+		Starting,
+		Playing,
+		Ending
+	}
+
+	public States CurrentStates;
+	
+	
+	
+	
+	public void OnSwitch () 
+	{
+		switch (CurrentStates)
+		{
+			case States.Starting:
+				StartingEvent.Invoke();
+				//work
+				break;
+			case States.Playing:
+				PlayingEvent.Invoke();
+				break;
+			case States.Ending:
+				EndingEvent.Invoke();
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		} 
+	}
+}
---
+++ FirstGame/Assets/Scripts/Weapons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour {
+	public UnityEvent FiringEvent, PowerFireEvent, EmptyAmmoEvent;
+
+
+	public enum States
+	{
+		Firing,
+		PowerFire,
+		EmptyAmmo,
+		
+	}
+
+	public States CurrentState;
+	
+	
+	
+	void Update () 
+	{
+		switch (CurrentState)
+		{
+			case States.Starting:
+				FiringEvent.Invoke();
+				break;
+			case States.Playing:
+				PowerFireEvent.Invoke();
+				break;
+			case States.Dying:
+				EmptyAmmoEvent.Invoke();
+				break;
+			
+			
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+}
