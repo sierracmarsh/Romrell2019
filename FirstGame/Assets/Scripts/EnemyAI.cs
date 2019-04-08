e9159d0f4a9b65a68983a82dd7f8bdce54890c8f
Sierra Marsh <ierr.mar@gmail.com>
2019-04-08 12:09:51 -0600

    UI

---
+++ FirstGame/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class EnemyAI : MonoBehaviour
+{
+	public Transform Jim;
+	private NavMeshAgent agent;
+	// Use this for initialization
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+
+	void OnTriggerStay(Collider other)
+	{
+		agent.destination = Jim.position;
+	}
+}
