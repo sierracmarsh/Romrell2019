e79025756ca08a5a828208300e517ff8904697e9
Sierra Marsh <ierr.mar@gmail.com>
2019-04-10 11:09:31 -0600

    Counter
    Did I mess up git?!

---
+++ FirstGame/Assets/Scripts/Counter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Counter : MonoBehaviour
+{
+	public int Value = 6, MinValue = 3;
+	public float WaitTime = 1;
+
+	public UnityEvent OnCountEvent, OnEndEvent;
+
+	public void StartCounter()
+	{
+		StartCoroutine(RunCounter());
+	}
+
+	private IEnumerator RunCounter() 
+	{
+		var waitObject = new WaitForSeconds(WaitTime);
+		
+		while (Value > MinValue)
+		{
+			yield return waitObject;
+			OnCountEvent.Invoke();
+			Value--;
+		}
+
+		yield return waitObject;
+		OnEndEvent.Invoke();
+	}
+	
+	
+		
+	
+}
